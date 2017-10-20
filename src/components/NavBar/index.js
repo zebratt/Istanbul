88d1b137fe4f94fcf3a28084467506185b353eab
xuxuejian <jearbilove@yeah.net>
2017-10-20 17:07:45 +0800

    update

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,19 @@
+/**
+ * Created by xuejian.xu on 2017/10/20.
+ */
+
+import './style.scss';
+import React, {Component} from 'react';
+
+class NavBar extends Component{
+
+  render(){
+    return (
+      <div id="NavBar">
+
+      </div>
+    )
+  }
+}
+
+export default NavBar;
