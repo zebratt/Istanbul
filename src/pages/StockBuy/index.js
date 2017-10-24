a53f56deb7813594853af08ae2411ea71ed7f024
xuejian.xu <jearbilove@yeah.net>
2017-10-24 22:08:10 +0800

    update

--- src/pages/StockBuy/index.js
+++ src/pages/StockBuy/index.js
@@ -116,6 +116,70 @@ class StockBuy extends Component {
                 <li>30万</li>
                 <li>50万</li>
               </ul>
+              <div className="efficiency">资金使用率 可买入-股，资金利用率-%</div>
+              <div className="hold-time">
+                <div className="hold-time-left">
+                  <span>持仓时间</span>
+                </div>
+                <div className="hold-time-right">
+                  <div className="item">T+1|D</div>
+                </div>
+              </div>
+              <div className="hold-time">
+                <div className="hold-time-left">
+                  <span>触发止盈</span>
+                </div>
+                <div className="hold-time-right">
+                  <div className="item">5000</div>
+                </div>
+              </div>
+              <div className="hold-time">
+                <div className="hold-time-left">
+                  <span>触发止损</span>
+                </div>
+              </div>
+              <ul className="amounts">
+                <li>-1000</li>
+                <li>-1300</li>
+                <li>-1700</li>
+              </ul>
+              <div className="hold-time">
+                <div className="hold-time-left">
+                  <span>交易综合费</span>
+                </div>
+                <div className="hold-time-right">
+                  <strong>45</strong>元（包括前两日）
+                </div>
+              </div>
+              <div className="hold-time">
+                <div className="hold-time-left">
+                  <span>履约保证金</span>
+                </div>
+                <div className="hold-time-right">
+                  <strong>1250</strong>元
+                </div>
+              </div>
+              <div className="hold-time">
+                <div className="hold-time-left">
+                  <span>递延条件</span>
+                </div>
+                <div className="hold-time-right">
+                  浮动盈亏大于<em>-650</em>
+                </div>
+              </div>
+              <div className="hold-time">
+                <div className="hold-time-left">
+                  <span>递延费</span>
+                </div>
+                <div className="hold-time-right">
+                  <em>18</em>元/天
+                </div>
+              </div>
+              <div>
+                <input type="checkbox"/>
+                <span>我已阅读并签署以下协议</span>
+              </div>
+              <button className="btn-buy">点买</button>
             </div>
           </div>
         </div>
