a4593377a098c2e31a4e4ff94ad2c4ac2f561f0d
xuejian.xu <jearbilove@yeah.net>
2017-11-06 21:56:39 +0800

    update

--- src/pages/StockBuy/modules/sell.js
+++ src/pages/StockBuy/modules/sell.js
@@ -8,7 +8,7 @@ class Sell extends Component{
   render(){
     return (
       <div className="body">
-        Sell tab
+        <div className="tip">当前持仓所需递延费<span className="red">0</span>元(周末及节假日免费),持仓盈利总计：元</div>
       </div>
     )
   }
