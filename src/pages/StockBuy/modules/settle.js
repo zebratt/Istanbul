e717383006b802f31c48527d10e336b00e601fd7
xuejian.xu <jearbilove@yeah.net>
2017-11-13 23:05:42 +0800

    update

--- src/pages/StockBuy/modules/settle.js
+++ src/pages/StockBuy/modules/settle.js
@@ -5,10 +5,80 @@
 import React, {Component} from 'react';
 
 class Settle extends Component{
+  componentDidMount(){
+    const {customerId, token, getClinchRecord } = this.props;
+
+    if(!customerId){
+      return ;
+    }
+
+    getClinchRecord(customerId, token);
+  }
+
   render(){
+    const {customerId, token, historyData} = this.props;
+    const {content} = historyData;
+
+    if(!customerId){
+      return (
+        <div className="body">
+          <div>请先登录!</div>
+        </div>
+      )
+    }
+
+    if(!content){
+      return null;
+    }
+
     return (
       <div className="body">
-        Settle tab
+        <table className="table">
+          <thead>
+            <tr className="title">
+              <td>成交记录ID</td>
+              <td>委托ID</td>
+              <td>股票代码</td>
+              <td>股票名称</td>
+              <td>交易方向</td>
+              <td>成交时间</td>
+              <td>成交价格</td>
+              <td>成交数量</td>
+              <td>成交总额</td>
+              <td>交易费</td>
+              <td>印花税</td>
+              <td>过户费</td>
+              <td>其他费用</td>
+              <td>备注信息</td>
+              <td>持仓ID</td>
+              <td>方案ID</td>
+            </tr>
+          </thead>
+          <tbody>
+          {content.map((item)=>{
+            return (
+              <tr key={item.clinchRecordId}>
+                <td>{item.clinchRecordId}</td>
+                <td>{item.entrustId}</td>
+                <td>{item.stockCode}</td>
+                <td>{item.stockName}</td>
+                <td>{item.tradeDirection}</td>
+                <td>{item.clinchTime}</td>
+                <td>{item.clinchPrice}</td>
+                <td>{item.clinchNum}</td>
+                <td>{item.totalValue}</td>
+                <td>{item.transactionFee}</td>
+                <td>{item.stampDuty}</td>
+                <td>{item.transferFee}</td>
+                <td>{item.otherCharges}</td>
+                <td>{item.remark}</td>
+                <td>{item.positionId}</td>
+                <td>{item.schemeId}</td>
+              </tr>
+            )
+          })}
+          </tbody>
+        </table>
       </div>
     )
   }
